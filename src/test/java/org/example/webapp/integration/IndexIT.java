b2c4ec848c26059b12115ac351dd2727c7b73346
Paul Vorbach <paul@vorba.ch>
2016-12-01 00:50:53 +0100

    Automate integration tests

--- src/test/java/org/example/webapp/integration/IndexIT.java
+++ src/test/java/org/example/webapp/integration/IndexIT.java
@@ -23,6 +23,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -31,15 +32,18 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ITConfig.class)
-public class IndexTest {
+public class IndexIT {
 
     @Autowired
     private WebDriver webDriver;
 
+    @Value("${server.port}")
+    private int serverPort;
+
     @Test
     public void visitIndexPage() throws Exception {
 
-        webDriver.get("http://localhost:8080/");
+        webDriver.get(String.format("http://localhost:%d/", serverPort));
         WebElement working = webDriver.findElement(By.id("working"));
 
         Assert.assertThat(working.getText(), is(equalTo("It works!")));
