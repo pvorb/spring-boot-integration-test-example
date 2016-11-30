20c3abb04fec1321e577edd9ed81b8f1d508c4e7
Paul Vorbach <paul@vorba.ch>
2016-11-30 23:47:18 +0100

    Manual integration test

---
+++ src/test/java/org/example/webapp/integration/IndexTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.webapp.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ITConfig.class)
+public class IndexTest {
+
+    @Autowired
+    private WebDriver webDriver;
+
+    @Test
+    public void visitIndexPage() throws Exception {
+
+        webDriver.get("http://localhost:8080/");
+        WebElement working = webDriver.findElement(By.id("working"));
+
+        Assert.assertThat(working.getText(), is(equalTo("It works!")));
+    }
+}
